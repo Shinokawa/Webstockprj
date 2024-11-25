7fd0735d17bdd371d29375b8f8750e4d19117314
Sakiko <liangrongguang.tony@gmail.com>
2024-11-25 10:05:28 +0800

    initial commit

---
+++ syc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, C++ environment is working!" << endl;
+
+    // 检测输入功能是否正常
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // 简单的加法运算
+    cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+
+    return 0;
+}
+//hhhvhhhh
