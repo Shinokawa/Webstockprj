7f0e23c054f19a6561fc2916a55efb69247b34e7
Sakiko <liangrongguang.tony@gmail.com>
2024-11-27 13:49:38 +0800

    Windows平台API库

---
+++ build/CMakeFiles/WEBSO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WEBSO.dir/src/CTPQuoteDemo.cpp.o"
+  "CMakeFiles/WEBSO.dir/src/CTPQuoteDemo.cpp.o.d"
+  "WEBSO"
+  "WEBSO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WEBSO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/WEBSO.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WEBSO.
+# This may be replaced when dependencies are built.
